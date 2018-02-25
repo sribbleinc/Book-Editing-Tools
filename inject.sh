d889fdab84d1c189d5d63ddbf637ea45680d1ab6
sribbleinc <sribbleinc@berkeley.edu>
2018-02-25 01:40:59 -0800

    fixed script to account for windows

--- inject.sh
+++ inject.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 vars=$(cat vars.txt)
 target=$(pwd)
+#Windows Hack; if you're home directory isn't C: you're smart enough to modify the script.
+if [[ $target == /C:* ]]; then
+	target=$(echo $target | cut -c2-)
+fi
 cp comicautoformat_template.py comicautoformat.py
 perl -i -pe "s|REPLACE|$vars|" comicautoformat.py
 perl -i -pe "s|_target_|$target|" comicautoformat.py
